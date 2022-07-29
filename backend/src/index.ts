749e94d4637a8ecde8621e6a90e0382fd39afe8c
tomast1337 <vycasnicolas@gmail.com>
2022-07-28 21:51:34 -0300

    Backend Init

---
+++ backend/src/index.ts
@@ -0,0 +1,15 @@
+import express from 'express';
+
+const PORT: string = "3000" || process.env.PORT;
+
+const server: express.Application = express();
+
+
+// 404 handler
+server.use((req, res, next) => {
+    res.status(404).send("404: Page not found");
+});
+
+server.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
