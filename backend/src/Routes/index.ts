0c960ca71270acd97d6417ab403304fc5b953441
tomast1337 <vycasnicolas@gmail.com>
2022-08-05 22:09:02 -0300

    Adicionando rotas no express

--- backend/src/Routes/index.ts
+++ backend/src/Routes/index.ts
@@ -1,10 +1,13 @@
 import LoginRoutes from './login';
+import AdminRoutes from './admin';
 
 import express from 'express';
 const router: express.Router = express.Router();
 
 // Login
 router.use('/login', LoginRoutes);
+// Admin
+router.use('/admin', AdminRoutes);
 
 
 export default router;
