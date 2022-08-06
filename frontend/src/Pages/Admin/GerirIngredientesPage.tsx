6b4c97be1f3c96b9fa8cbeacafc06cdb0f74f57d
tomast1337 <vycasnicolas@gmail.com>
2022-08-06 00:08:34 -0300

    Adicionado NavBar a telas do admin

--- frontend/src/Pages/Admin/GerirIngredientesPage.tsx
+++ frontend/src/Pages/Admin/GerirIngredientesPage.tsx
@@ -1,10 +1,12 @@
 import * as React from "react";
 import { useDispatch, useSelector } from "react-redux";
 import styles from "./GerirIngredientesPage.module.scss";
+import MenuNav from "../../Components/Admin/AdminNavbar";
 
 const GerirIngredientesPage = () => {
     return (
         <>
+            <MenuNav />
         </>
     );
 }
--- frontend/src/Pages/Admin/GerirProdutosPage.tsx
+++ frontend/src/Pages/Admin/GerirProdutosPage.tsx
@@ -1,10 +1,12 @@
 import * as React from "react";
 import { useDispatch, useSelector } from "react-redux";
 import styles from "./GerirProdutosPage.module.scss";
+import MenuNav from "../../Components/Admin/AdminNavbar";
 
 const GerirProdutosPage = () => {
     return (
         <>
+            <MenuNav />
         </>
     );
 }
